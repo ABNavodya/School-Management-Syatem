2fddf9547e9a7b1687ba5c80ebe82c78eea297e3
AB Navodya <buddhininavodya34@gmail.com>
2023-08-25 13:49:49 +0530

    update questions js file

--- main.js
+++ main.js
@@ -4,3 +4,21 @@ window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle
     ('window-scroll', window.scrollY > 0)
 })
+
+// show or hide faq answer
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq=> {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        //change icon
+        const icon = faq.querySelector('.faq_icon i');
+        if(icon.className === 'uil uil-plus') {
+            icon.className = "uil uil-minus"
+        } else {
+            icon.className = "uil uil-plus"
+        }
+    })
+})
